956f566853f5d217c3c0582bddf7da32808ebf2c
farge clement <farge.clement@gmail.com>
2015-12-15 00:08:44 +0100

    salut

--- app/src/main/java/fr/virtualtrails/configureRoute/CConfigureRouteCtrl.java
+++ app/src/main/java/fr/virtualtrails/configureRoute/CConfigureRouteCtrl.java
@@ -92,7 +92,7 @@ public class CConfigureRouteCtrl extends AppCompatActivity {
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                
+
             }
         });
 
